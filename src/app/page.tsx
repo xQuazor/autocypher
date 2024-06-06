2381b39b98d35b89f5d04c0eafe805235ce2a997
dovydas <dovydas.vilkevicius@gmail.com>
2024-06-06 18:07:55 +0100

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,14 @@ import utilities from "../scss/utlities.module.scss";
 import Image from "next/image";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import FlightIcon from "@mui/icons-material/Flight";
+import { Navigation } from "swiper/modules";
 import { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
 export default function Home() {
   return (
     <main className={styles.container}>
@@ -31,9 +35,9 @@ function Section1() {
         <p className={typography.heading3 + " " + utilities.margin_bot}>
           A Robust Approach
         </p>
-        <p className={typography.paragraph__grey}>
+        <a href={"#section2"} className={typography.paragraph__grey}>
           Learn more <KeyboardArrowDownIcon />
-        </p>
+        </a>
       </div>
       <div className={styles.section1Right}>
         {images.map((number) => {
@@ -55,11 +59,11 @@ function Section1() {
 
 function Section2() {
   return (
-    <section className={styles.section2}>
+    <section id={"section2"} className={styles.section2}>
       <LineAndBoxesAnimation />
       <Image
         className={styles.section2Background}
-        src={"/home/section2.webp"}
+        src={"/home/section2.png"}
         alt={"Background Image"}
         fill={true}
         style={{ objectFit: "cover" }}
@@ -69,30 +73,60 @@ function Section2() {
 }
 
 function Section3() {
-  return <section></section>;
+  return (
+    <section className={styles.section3}>
+      <div className={styles.section3Left}>
+        <h3 className={typography.heading3 + " " + utilities.margin_bot}>Project Objectives</h3>
+        <ol className={styles.section3LeftList}>
+          <li className={typography.paragraph}>
+            Reduce the conservatism by moving away from the worst-case behaviour
+            of the uncertainty and including the statistics of the uncertainty
+            in the analysis.
+          </li>
+          <li className={typography.paragraph}>
+            Relax the need for having a precise mathematical description of the
+            cyber-physical system.
+          </li>
+          <li className={typography.paragraph}>
+            Adapt the design for trade-offs between various performance
+            criteria.
+          </li>
+          <li className={typography.paragraph}>
+            Mitigate the exponential computational complexity to handle
+            large-scale cyber-physical systems.
+          </li>
+        </ol>
+      </div>
+        <div className={styles.section3Right}>
+            <Image alt={"illustration"} src={"/home/section3.png"} fill={true}    style={{ objectFit: "contain" }}/>
+        </div>
+    </section>
+  );
 }
 const LineAndBoxesAnimation = () => {
   const lineRef = useRef(null);
   const boxRefs = useRef([]);
   const containerRef = useRef(null);
-  gsap.registerPlugin(ScrollTrigger);
+  const circleRef = useRef([]);
   boxRefs.current = [];
+  circleRef.current = [];
+  gsap.registerPlugin(ScrollTrigger);
+
   useEffect(() => {
     const viewportHeight = window.innerHeight;
-    const totalScrollAmount = 1.35 * viewportHeight;
-    // Ensure the full page height is used for scroll calculations
+    const totalScrollAmount = 1.35 * window.innerHeight;
+
     ScrollTrigger.create({
       trigger: containerRef.current,
       start: `top bottom-=${0.35 * viewportHeight}`,
       end: totalScrollAmount,
       onUpdate: (self) => {
-        // Line height adjusted according to the progress through 200vh
-        const lineHeight = self.progress * totalScrollAmount;
-        gsap.set(lineRef.current, { height: `${lineHeight}px` });
+        gsap.set(lineRef.current, {
+          height: `${self.progress * totalScrollAmount}px`,
+          maxHeight: `${0.68 * totalScrollAmount}px`,
+        });
       },
     });
-
-    // Animate the boxes sliding into view
     boxRefs.current.forEach((box, index) => {
       gsap.fromTo(
         box,
@@ -102,19 +136,32 @@ const LineAndBoxesAnimation = () => {
           autoAlpha: 1,
           scrollTrigger: {
             trigger: box,
-            start: "top center",
+            start: `top bottom-=${0.28 * viewportHeight}`,
             toggleActions: "play none none reverse",
           },
         },
       );
     });
-    // Make sure to kill ScrollTrigger instances on component unmount to prevent memory leaks.
+    circleRef.current.forEach((circle, index) => {
+      gsap.fromTo(
+        circle,
+        { scale: 0, autoAlpha: 0 },
+        {
+          scale: 1,
+          autoAlpha: 1,
+          scrollTrigger: {
+            trigger: circle,
+            start: `top bottom-=${0.28 * viewportHeight}`,
+            toggleActions: "play none none reverse",
+            markers: true,
+          },
+        },
+      );
+    });
     return () => {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
   }, []);
-
-  // Helper function to add refs
   const addBoxRef = (el: any) => {
     // @ts-ignore
     if (el && !boxRefs.current.includes(el)) {
@@ -123,14 +170,32 @@ const LineAndBoxesAnimation = () => {
     }
   };
 
+  const addCircleRef = (el: any) => {
+    // @ts-ignore
+    if (el && !circleRef.current.includes(el)) {
+      // @ts-ignore
+      circleRef.current.push(el);
+    }
+  };
+
   return (
     <section className={styles.section2} ref={containerRef}>
       <div className={styles.section2Backdrop}></div>
       <div ref={lineRef} className={styles.section2Line}></div>
       <div
+        ref={addCircleRef}
+        className={styles.section2Circle}
+        style={{ top: "25vh" }}
+      ></div>
+      <div
+        ref={addCircleRef}
+        className={styles.section2Circle}
+        style={{ top: "60vh" }}
+      ></div>
+      <div
         className={styles.section2Box}
         ref={addBoxRef}
-        style={{ top: "25vh", left: "55%" }}
+        style={{ top: "15vh", left: "55%" }}
       >
         <h3 className={typography.heading3 + " " + utilities.margin_bot_small}>
           What are Cyber-Physical Systems?
@@ -145,7 +210,7 @@ const LineAndBoxesAnimation = () => {
       <div
         className={styles.section2Box}
         ref={addBoxRef}
-        style={{ top: "60vh", right: "55%" }}
+        style={{ top: "50vh", right: "55%" }}
       >
         <h3 className={typography.heading3 + " " + utilities.margin_bot_small}>
           Issues with Control Software
@@ -156,22 +221,12 @@ const LineAndBoxesAnimation = () => {
           particularly in safety-critical applications.
         </p>
       </div>
-      <h3
-        className={typography.heading3 + " " + utilities.margin_bot_small}
-        style={{
-          position: "absolute",
-          top: "90vh",
-          left: "50%",
-          transform: "-50% -50%",
-        }}
-      >
-        Real World Examples of CPS failure
-      </h3>
-      <CPSFailureBox
-        text={
-          "Crash of airplanes due to software bugs (Boeing 737 Max, costed the company €15.9 billion)"
-        }
-      />
+      <div className={styles.section2InfoBoxes}>
+        <h3 className={typography.heading3 + " " + utilities.margin_bot}>
+          Real World Examples of CPS failure
+        </h3>
+        <Carousel />
+      </div>
     </section>
   );
 };
@@ -179,13 +234,45 @@ const LineAndBoxesAnimation = () => {
 // @ts-ignore
 function CPSFailureBox({ text }) {
   return (
-    <div className={styles.section2PositionBoxes}>
-      <div className={styles.section2FailureBox}>
-        <p className={typography.paragraph__white}>{text}</p>
-        <div className={styles.section2FailureBoxIcon}>
-          <FlightIcon sx={{ fontSize: 36 }} />
-        </div>
+    <div className={styles.section2FailureBox}>
+      <p className={typography.paragraph__white}>{text}</p>
+      <div className={styles.section2FailureBoxIcon}>
+        <FlightIcon sx={{ fontSize: 36 }} />
       </div>
     </div>
+  );
+}
+function Carousel() {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      modules={[Navigation]}
+      navigation
+      centeredSlides={true}
+      className={styles.section2Carousel}
+    >
+      <SwiperSlide className={styles.section2CarouselSlide}>
+        <CPSFailureBox
+          text={
+            "Crash of airplanes due to software bugs (Boeing 737 Max, costed the company €15.9 billion)"
+          }
+        />
+      </SwiperSlide>
+      <SwiperSlide className={styles.section2CarouselSlide}>
+        <CPSFailureBox
+          text={
+            "Crash of airplanes due to software bugs (Boeing 737 Max, costed the company €15.9 billion)"
+          }
+        />
+      </SwiperSlide>
+      <SwiperSlide className={styles.section2CarouselSlide}>
+        <CPSFailureBox
+          text={
+            "Crash of airplanes due to software bugs (Boeing 737 Max, costed the company €15.9 billion)"
+          }
+        />
+      </SwiperSlide>
+    </Swiper>
   );
 }
--- src/components/footer/component.tsx
+++ src/components/footer/component.tsx
@@ -1,4 +1,38 @@
-
+import styles from "./component.module.scss";
+import typography from "../../scss/typography.module.scss";
+import Image from "next/image";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import XIcon from "@mui/icons-material/X";
 export default function Footer() {
-    return <footer></footer>;
+  return (
+    <footer className={styles.container}>
+      <div className={styles.containerTop}>
+        <div className={styles.containerTopLeft}>
+          <div className={styles.containerFlag}>
+            <Image
+                alt={"EU Flag"}
+                src={"/footer/flag_eu.webp"}
+                fill={true}
+                style={{objectFit: "contain"}}
+            />
+          </div>
+          <p className={typography.footer__white}>
+            Funded by the European Union under the Grant Agreement Number
+            101089047.
+          </p>
+        </div>
+        <div className={styles.containerTopRight}>
+          <LinkedInIcon htmlColor={"#FAF5F8"} sx={{fontSize: 40}}/>
+          <XIcon htmlColor={"#FAF5F8"} sx={{fontSize: 40}}/>
+        </div>
+
+      </div>
+      <div className={styles.containerBottom}>
+        <p className={typography.footer__white}>
+          © Copyright 2024 by MPI-SWS
+        </p>
+        <p className={typography.footer__white}>Privacy Policy</p>
+      </div>
+    </footer>
+  );
 }
